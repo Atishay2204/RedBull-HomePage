6bbc62d851cca94dcdd7873be48b9c33ce3460f0
Atishay Jain <Atishayanupam@gmail.com>
2026-09-12 23:08:34 +0530

    Update app and deployment configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+clearimport { useEffect } from 'react';
 import { ThemeProvider } from './context/ThemeContext';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
