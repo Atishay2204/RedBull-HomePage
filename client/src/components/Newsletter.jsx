ef168760313de5b1a8f721b0aedf46b4988086f2
Atishay Jain <Atishayanupam@gmail.com>
2026-09-13 01:04:29 +0530

    Updated api

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -12,7 +12,7 @@ export default function Newsletter() {
 
     setStatus('loading');
     try {
-      const res = await fetch('/api/newsletter', {
+      const res = await fetch('/https://redbull-homepage-backend.onrender.com/newsletter', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email }),
