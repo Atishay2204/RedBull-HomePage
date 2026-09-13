90713001a38c9de0f1fc72d7bcc9d34f44f64111
Atishay Jain <Atishayanupam@gmail.com>
2026-09-13 14:41:01 +0530

    backend fixed

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -12,7 +12,7 @@ export default function Newsletter() {
 
     setStatus('loading');
     try {
-      const res = await fetch('https://redbull-homepage-backend.onrender.com/newsletter', {
+      const res = await fetch('https://redbull-homepage-backend.onrender.com/api/newsletter', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email }),
